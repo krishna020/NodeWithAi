16a0155dff4fcca68dfbcd0a6b62e8cb39e681ef
krishnaAugtrans <krishna.sahu@augtrans.com>
2025-08-01 00:54:37 +0530

    adding swagger

--- src/server.js
+++ src/server.js
@@ -1,26 +1,27 @@
-const http= require('http')
-const express=require('express')
-const dotenv =require('dotenv')
-const path=require('path')
-const user= require('./controllers/user.controller.js')
+const http = require("http");
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const user = require("./controllers/user.controller.js");
 const bodyParser = require("body-parser");
-const connectDB = require('./db/conn.js');
+const connectDB = require("./db/conn.js");
+const { swaggerUi, swaggerSpec } = require("../swagger.js");
 dotenv.config();
 
-const app= express();
+const app = express();
 connectDB();
-dotenv.config({ path: path.join(__dirname, '.env') });
-const port= process.env.PORT ||5050;
-const hostname =process.env.HOST_NAME
 
-app.use(express.json())
+dotenv.config({ path: path.join(__dirname, ".env") });
+const port = process.env.PORT || 5050;
+const hostname = process.env.HOST_NAME;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-const options={}
+const options = {};
 
 const server = http.createServer(options, app).listen(port, hostname, () => {
-    console.log(`HTTP Server listening on https://${hostname}:${port}`);
+  console.log(`HTTP Server listening on https://${hostname}:${port}`);
 });
-app.use('/user/api/v1',user)
-
-
+app.use("/user/api/v1", user);
---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task API",
+      version: "1.0.0",
+      description: "API documentation for Task Manager",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to your route files for JSDoc comments
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+module.exports = { swaggerUi, swaggerSpec };
